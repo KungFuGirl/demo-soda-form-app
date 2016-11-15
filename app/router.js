58d82dff70c46003fdeaf6c852452604a0c6dfe0
Anna Krupp <cherchezlafemme@users.noreply.github.com>
2016-11-15 13:35:40 -0800

    Create 3 routes for different select option, basic index.hbs layout

--- app/router.js
+++ app/router.js
@@ -7,6 +7,9 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('power-select');
+  this.route('html-native');
+  this.route('emberx-select');
 });
 
 export default Router;
