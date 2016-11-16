01b2718b2197d9418b7f210439a55eb33bafa7e4
bambery <lwszolek@gmail.com>
2016-11-15 16:40:35 -0800

    added data type for clarification of intent

--- app/models/flavor.js
+++ app/models/flavor.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr();
-  sodas: DS.hasMany('soda');
+  name: DS.attr('string'),
+  sodas: DS.hasMany('soda')
 });
--- app/models/soda.js
+++ app/models/soda.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  name: DS.attr();
-  flavor: DS.belongsTo('flavor');
+  name: DS.attr('string'),
+  flavor: DS.belongsTo('flavor')
 });
