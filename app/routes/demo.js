63a255baac0f940d08d2aeb69470ad5d20eb018b
bambery <lwszolek@gmail.com>
2016-11-16 17:14:09 -0800

    passing data between routes

--- app/routes/demo.js
+++ app/routes/demo.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
+ 
+let flavors = [{
+  id: 1,
+  name: "cherry"
+},{
+  id: 2,
+  name: "cream"
+},{
+  id: 3,
+  name: "orange"
+},{
+  id: 4,
+  name: "rootbeer"
+}];
 
 export default Ember.Route.extend({
+  model() {
+    return this.get('store').createRecord('soda');
+  },
+  flavors() {
+    return flavors;
+  }
 });
